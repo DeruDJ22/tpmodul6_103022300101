018812d7f414d899bda71070f335435c2411899c
Kuroakai <derudj12345@gmail.com>
2025-03-23 11:25:38 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300101/Program.cs
+++ tpmodul6_103022300101/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul6_103022300101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - Deru Khairan Djuharianto");
+            video.PrintVideoDetails();
+            Console.WriteLine();
+            video.IncreasePlayCount(777777777);
+            video.PrintVideoDetails();
+        }
+    }
+}
---
+++ tpmodul6_103022300101/SayaTubeVideo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_103022300101
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+        Random ran = new Random();
+
+        public SayaTubeVideo(string title)
+        {
+            this.id = ran.Next(1000,9999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Judul: {title}");
+            Console.WriteLine($"Jumlah diputar: {playCount}");
+        }
+    }
+}
